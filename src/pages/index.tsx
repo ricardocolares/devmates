438146e98b837bdc27bafebdec44f37e4e24529b
Ricardo Colares <ricardo.colares@hp.com>
2022-05-23 16:20:09 -0300

    chore: styled components configured

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import styled from "styled-components";
+
 export default function Home() {
+  const Title = styled.h1`
+    color: #f54;
+  `;
+
   return (
     <div className="container">
-      <h1>Home</h1>
+      <Title>Home</Title>
     </div>
-  )
+  );
 }
